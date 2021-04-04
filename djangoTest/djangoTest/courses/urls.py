7ff53e3296f81d9baf93cf6885ad02a80c3ffb28
Giovana Thais <giovana.carvalho@ma9.com.br>
2021-04-04 18:58:22 -0300

    refactor(melhorias)

--- djangoTest/djangoTest/courses/urls.py
+++ djangoTest/djangoTest/courses/urls.py
@@ -3,8 +3,8 @@ from .views import CourseList,CourseCreate,CourseUpdate,CourseDelete
 
 
 urlpatterns = [
-    path('',CourseList.as_view(),name='cursos_list'),
-    path('create/',CourseCreate.as_view(),name='cursos_create')
-    path('update/<pk::int>/',CourseUpdate.as_view(),name='cursos_update')
-    path('delete/<pk::int>/',CourseDelete.as_view(),name='cursos_delete')
+    path('',CourseList.as_view(),name='course_list'),
+    path('create/',CourseCreate.as_view(),name='course_create')
+    path('update/<pk::int>/',CourseUpdate.as_view(),name='course_update')
+    path('delete/<pk::int>/',CourseDelete.as_view(),name='course_delete')
 ] 
